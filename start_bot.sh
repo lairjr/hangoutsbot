d09815f24a2f39bf2b6a326a6fc9f1bda91a5294
lairjr <lairjr@outlook.com>
2017-07-12 16:41:20 -0300

    Adapt start script to server

--- start_bot.sh
+++ start_bot.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-ETHERNET_INTERFACE='en0'
-SERVER_IP=$(/sbin/ifconfig $ETHERNET_INTERFACE | grep 'inet' | cut -d: -f2 | awk '{ print $2 }')
+ETHERNET_INTERFACE='eno1'
+SERVER_IP=$(/sbin/ifconfig $ETHERNET_INTERFACE | grep 'inet addr' | cut -d: -f2 | awk '{ print $1; }')
 
 docker build -t hangoutsbot/hangoutsbot .
-docker run -e HOST_IP=$SERVER_IP -it --rm hangoutsbot/hangoutsbot
+docker run -e HOST_IP=$SERVER_IP --network="bridge" -it --rm hangoutsbot/hangoutsbot
