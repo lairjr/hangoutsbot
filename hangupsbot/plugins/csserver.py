8d6a4ec285d0b16146156863b63e9c9306a45114
lairjr <lairjr@outlook.com>
2017-07-12 16:11:57 -0300

    Rename serverip to csserver

--- hangupsbot/plugins/csserver.py
+++ hangupsbot/plugins/csserver.py
@@ -3,13 +3,13 @@ import plugins
 import os
 
 def _initialise(bot):
-    plugins.register_user_command(["serverip"])
+    plugins.register_user_command(["cs_connect"])
 
-def serverip(bot, event, *args):
+def cs_connect(bot, event, *args):
     """
     Display CS server ip
 
-    /bot serverip
+    /bot cs_connect
     """
 
     yield from bot.coro_send_message(event.conv, "connect " + str(os.environ['HOST_IP']).strip())
