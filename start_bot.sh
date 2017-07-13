b8fc70744ba54f3af82f7d863e34556b427a853d
lairjr <lairjr@outlook.com>
2017-07-13 10:40:23 -0300

    Adjust start bot script do deatach process

--- start_bot.sh
+++ start_bot.sh
@@ -4,4 +4,4 @@ ETHERNET_INTERFACE='eno1'
 SERVER_IP=$(/sbin/ifconfig $ETHERNET_INTERFACE | grep 'inet addr' | cut -d: -f2 | awk '{ print $1; }')
 
 docker build -t hangoutsbot/hangoutsbot .
-docker run -e HOST_IP=$SERVER_IP --network="bridge" -it --rm hangoutsbot/hangoutsbot
+docker run -d -e HOST_IP=$SERVER_IP --network="bridge" -it --rm hangoutsbot/hangoutsbot
